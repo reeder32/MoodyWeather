f2d728585dcc39a9df60cb974a791323747da6ef
Nicholas Reeder <nickreeder32@gmail.com>
2020-03-23 11:47:47 -0700

    Initial Commit

---
+++ MoodyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoodyWeather
+//
+//  Created by Nicholas Reeder on 3/23/20.
+//  Copyright © 2020 Nicholas Reeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
